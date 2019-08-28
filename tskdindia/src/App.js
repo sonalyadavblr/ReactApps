4f391684345052a9186ca0ef10455f46c0acf222
Sonal Yadav <sonalyadav.blr@gmail.com>
2019-08-28 18:00:02 +0530

    TSKDIndia project has been created

---
+++ tskdindia/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import Home from './components/home';
+ 
+import './App.css';
+
+function App() {
+  return (
+    <Home/>
+    
+  );
+}
+
+export default App;
---
+++ tskdindia/src/components/appBody.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class AppBody extends Component{
+    render() {
+        return (
+            <h1>AppBody</h1>
+        );
+    }
+}
